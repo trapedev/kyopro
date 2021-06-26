bbbdbb63eff1713697c4583af2e33429a4599b80
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-06-27 01:30:04 +0900

    配列の中央値を探す関数

---
+++ Others/median.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i = 0; i < (n); i++)
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+using Graph = vector<vector<int>>;
+
+//配列の要素の中央値を求める関数
+int median(vector<int> &v)
+{
+    size_t n = v.size() / 2;
+    nth_element(v.begin(), v.begin()+n, v.end());
+    return v[n];
+}
+
+int main(){
+    return 0;
+}
