b3e2e1eb93f62c5eaa24d554b72b5e97bde53675
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-08-17 10:53:19 +0900

    base code

--- base.cpp
+++ base.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
+
 #define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
 using ll = long long;
 using namespace std;
 using P = pair<int, int>;
-const ll mod = 1e9 + 7;
-const int INF = 1000000001;
 using Graph = vector<vector<int>>;
 
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+void solve(){
+
+}
+
 int main(){
+    
     return 0;
 }
